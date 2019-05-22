18418c27a4457d98fc986dd000802a7a0b6536ce
Scott Hall <smhall316@gmail.com>
2019-05-21 20:52:04 -0400

    Add Bayes decision theory with risk minimization

--- bayes/bayes2/bayes2_min_risk.py
+++ bayes/bayes2/bayes2_min_risk.py
@@ -22,7 +22,7 @@ from patreg.statistics.pdf import pdf
 def bayes2_min_risk(x: float, pw1: float, pw2: float,
                     pxw1_x: np.array, pxw1_y: np.array,
                     pxw2_x: np.array, pxw2_y: np.array,
-                    lam_12: float   , lam_21: float) -> (int, float, float):
+                    L: np.matrix) -> (int, float, float):
     """
     Calculate the probability that random variable x is in class w1 or w2.  This
     also will calculate the probability of error on the class selection.
@@ -37,6 +37,7 @@ def bayes2_min_risk(x: float, pw1: float, pw2: float,
     :param np.array pxw2_x: The x-component of the conditional probability
     density function p(x|w2).  This is the likelihood that r.v. x belongs w2
     :param np.array pxw2_y: The y-component of the conditional probability
+    :param float L: The loss/risk matrix used to make classification decision
     density function p(x|w2).  This is the likelihood that r.v. x belongs w2
     :rtype: float
     :raises AssertionError: If the x and y array lengths are not the same for
@@ -56,49 +57,78 @@ def bayes2_min_risk(x: float, pw1: float, pw2: float,
     px_pdf1 = pdf(x, x, pxw1_x, pxw1_y)
     px_pdf2 = pdf(x, x, pxw2_x, pxw2_y)
 
-    px = px_pdf1*pw1 + px_pdf2*pw2
+    # Calculate the likelihoods for each class
+    l1 = L[0,0]*px_pdf1*pw1 + L[1,0]*px_pdf2*pw2
+    l2 = L[0,1]*px_pdf1*pw1 + L[1,1]*px_pdf2*pw2
 
-    # Calculate the probability of x in class w1 and x in class w2
-    pw1x = px_pdf1*pw1/px
-    pw2x = px_pdf2*pw2/px
-
-    # Determine where the two classes intersect
-    idx = np.argwhere(np.diff(np.sign(pxw1_y - pxw2_y))).flatten()
-
-    # If multiple indicies intersect, find the index with the highest probability
-    # and set that as our xo boundary
-    xidx = -1
-    xmax = -1
-    for i in idx:
-        if pxw1_y[i] > xmax:
-            xidx = i
-            xmax = pxw1_y[i]
-
-    xo  = pxw1_x[xidx]
-
-    # Calculate the probability of commiting a decision error
-    Pe1 = 0
-    Pe2 = 0
-
-    for i in range(pxw2_x.size):
-        if pxw2_x[i] > xo:
-            break
-        if i == 0:
-            Pe1 = pxw2_y[i]
-        else:
-            Pe1 += (pxw2_x[i]-pxw2_x[i-1])*pxw2_y[i]
-
-    for i in range(pxw1_x.size):
-        if pxw2_x[i] < xo:
-            continue
-        if Pe2 == 0:
-            Pe2 = pxw1_y[i]
-        else:
-            Pe2 += (pxw1_x[i]-pxw1_x[i-1])*pxw1_y[i]
-
-    Pe = Pe1/2.0 + Pe2/2.0
-
-    if pw1x > pw2x:
-        return 1, pw1x, pw2x, Pe
+    if l1 < l2:
+        return 1, l1, l2
     else:
-        return 2, pw1x, pw2x, Pe
+        return 2, l1, l2
+
+if __name__=='__main__':
+
+    L = np.matrix('0 0.5; 1.0 0')
+
+    xw1,yw1 = normal.normal_distribution(5000, -20.0, 20.0, -5.0, 3.0)
+    xw2,yw2 = normal.normal_distribution(5000, -20.0, 20.0,  5.0, 3.0)
+
+    xclasses = []
+
+    # A priori probabilities
+    Pw1 = 0.5
+    Pw2 = 0.5
+
+    # List of random variales to classify
+    xs = [-5, 5, -1, 1, 0, -10, 10]
+
+    print(f'CLASSIFIERS FOR TWO NORMAL DISTRIBUTIONS')
+    for x in xs:
+        xclass,l1,l2 = bayes2_min_risk(x, Pw1, Pw2, xw1,yw1, xw2,yw2, L)
+        print(f'Random variable {x:6.2f} belongs to class {xclass}, l1={l1:.3f}, l2={l2:.3f}')
+        xclasses.append((x, xclass,l1,l2))
+
+    fig,ax = plt.subplots(1,2)
+    ax[0].plot(xw1,yw1, label='p(x|w1) - Class 1')
+    ax[0].plot(xw2,yw2, label='p(x|w2) - Class 2')
+    for (x,xclass,l1,l2) in xclasses:
+        ax[0].plot(x,0,'o', label=f'{x} in class {xclass}')
+    ax[0].set_xlim(left=-20,right=20)
+    ax[0].legend()
+    ax[0].set_title(f'Region Classifiers with Risk Minimization\nWith a priori probabilities of Pw1={Pw1} and Pw2={Pw2}')
+    ax[0].set_ylabel('p(x|w)')
+    ax[0].set_xlabel('x')
+    ax[0].grid()
+
+    print('\n\nCLASSIFIERS FOR GUMBEL AND CAUCHY DISTRIBUTIONS')
+
+    xw3,yw3 = gumbel.gumbel_distribution(5000, -20.0, 20.0, 0.5, 5.0)
+    xw4,yw4 = cauchy.cauchy_distribution(5000, -20.0, 20.0, 7.0, 1.0)
+
+    xclasses = []
+
+    # A priori probabilities
+    Pw1 = 0.5
+    Pw2 = 0.5
+
+    # List of random variales to classify
+    xs = [-5, 5, -1, 1, 0, -10, 10]
+
+    for x in xs:
+        xclass,l1,l2 = bayes2_min_risk(x, Pw1, Pw2, xw3,yw3, xw4,yw4, L)
+        print(f'Random variable {x:6.2f} belongs to class {xclass}, l1={l1:.3f}, l2={l2:.3f}')
+        xclasses.append((x, xclass,l1,l2))
+
+    ax[1].plot(xw3,yw3, label='p(x|w1) - Class 1')
+    ax[1].plot(xw4,yw4, label='p(x|w2) - Class 2')
+    for (x,xclass,l1,l2) in xclasses:
+        ax[1].plot(x,0,'o', label=f'{x} in class {xclass}')
+    ax[1].set_xlim(left=-20,right=20)
+    ax[1].legend()
+    ax[1].set_title(f'Region Classifiers with Risk Minimization\nWith a priori probabilities of Pw1={Pw1} and Pw2={Pw2}')
+    ax[1].set_ylabel('p(x|w)')
+    ax[1].set_xlabel('x')
+    ax[1].grid()
+
+    #plt.grid()
+    plt.show()
